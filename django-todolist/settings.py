87ac0cd88cdc3726ec3cbd7677da15b3d9ba31d8
hadi sql <hadi.sqalli@laplateforme.io>
2023-06-06 14:05:30 +0200

    added aos ainmation for tasks

--- django-todolist/settings.py
+++ django-todolist/settings.py
@@ -127,14 +127,14 @@ STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media')
+    # os.path.join(BASE_DIR, 'media')
 ]
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "mediafiles"
+# MEDIA_URL = "/media/"
+# MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
