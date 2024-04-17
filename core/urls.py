c5650bf848ecc9ffc6365ce3fc412fc1eacbe5bb
hadi sql <hadi.sqalli@laplateforme.io>
2024-04-17 17:37:12 +0200

    commit changes for self-hosting tests

--- core/urls.py
+++ core/urls.py
@@ -10,7 +10,7 @@ app_name = "core"
 urlpatterns = [
     path("", views.index, name="index"),
     path("signup/", views.signup, name="signup"),
-    path("login/", auth_views.LoginView.as_view(template_name="core/login.html",authentication_form=LoginForm), name="login"),
+    path("login/", auth_views.LoginView.as_view(template_name="core/login.html",authentication_form=LoginForm, next_page=reverse_lazy('tasks:index')), name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page=reverse_lazy('core:login')), name='logout'),
     path("profile/", views.profile_view, name="profile"),
     path("profile/update/", views.profile_update, name="profile_update"),
